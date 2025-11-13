7ebae2c9436ddaf50010db5e4eb63b26b5267b64
arantzaghg <arantzaghg@gmail.com>
2025-11-13 13:24:45 -0600

    Implement portfolio value calculation function

---
+++ portfolio_value.py
@@ -0,0 +1,23 @@
+from models import Operation
+
+def get_portfolio_value(cash: float, long_ops: list[Operation], short_ops: list[Operation],n_shares: float, y_ticker: float, x_ticker: float, y_p: float, x_p: float) -> float:
+    
+    port_val = cash
+
+    # add long positions value
+    for position in long_ops:
+        if position.ticker == x_ticker:
+            port_val += x_p * position.n_shares
+        if position.ticker == y_ticker:
+            port_val += y_p * position.n_shares
+
+    # add short positions value
+    for position in short_ops:
+        if position.ticker == x_ticker:
+            port_val += (position.entry_price - x_p) * position.n_shares
+        if position.ticker == y_ticker:
+            port_val += (position.entry_price - y_p) * position.n_shares
+
+    return port_val
+
+portfolio_value.py
